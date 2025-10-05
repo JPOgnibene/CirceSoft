6f79dc6ec6c8db494f6c6035eceb56390acc2aa4
Harrison Simpson <gharsimp@gmail.com>
2025-10-05 13:10:41 -0500

    Rudimentary coordinate exchange from frontend to backend
    Front end has button implemented on header that sends path to backend API endpoint. Current API path needs to be updated by front end and coordinates need to match what back end is expecting. Will complete next iteration.

--- Frontend/react/my-app/src/components/MapView.jsx
+++ Frontend/react/my-app/src/components/MapView.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
 import ClickToPath from "./ClickToPath";
 
-export default function MapView(sliderValue) {
+export default function MapView({ sliderValue, path, setPath }) {
   return (
-    <div style={{ width: "800px", height: "600px", border: "1px solid black" }}>
+    <div style={{ width: "100%", height: "100%", border: "1px solid black" }}>
       <TransformWrapper
         initialScale={1}
         minScale={0.5}
@@ -15,7 +15,8 @@ export default function MapView(sliderValue) {
         {({ state }) => (
         <TransformComponent>
           {/*<img src="/imagefrombackend/aerialview.png" alt="Aerial Map" width="800" />*/}
-          <ClickToPath pathProgress={sliderValue} />
+
+          <ClickToPath pathProgress={sliderValue} path={path} setPath={setPath} />
         </TransformComponent>
         )}
       </TransformWrapper>
